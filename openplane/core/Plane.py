7cc6f98110a6e5ce5e98771573f172dd5f887a5d
Louis Etienne <louis.etienne82@orange.fr>
2015-09-12 19:14:33 +0200

    Ajout des valeurs de centrage et optimisation

--- openplane/core/Plane.py
+++ openplane/core/Plane.py
@@ -9,7 +9,7 @@ import json
 class Plane:
 
     def __init__(self, values=None):
-        if values is not None and len(values) == 44:
+        if values is not None and len(values) == 64:
             self.create_plane(values)
 
     def create_plane(self, values):
@@ -67,6 +67,31 @@ class Plane:
             "Y": values[43]
         }
 
+        self.p1x = values[44]
+        self.p1y = values[45]
+        self.p2x = values[46]
+        self.p2y = values[47]
+        self.p3x = values[48]
+        self.p3y = values[49]
+        self.p4x = values[50]
+        self.p4y = values[51]
+        self.p5x = values[52]
+        self.p5y = values[53]
+
+        self.up1x = values[54]
+        self.up1y = values[55]
+        self.up2x = values[56]
+        self.up2y = values[57]
+        self.up3x = values[58]
+        self.up3y = values[59]
+        self.up4x = values[60]
+        self.up4y = values[61]
+        self.up5x = values[62]
+        self.up5y = values[63]
+
+
+
+
     def save_plane(self):
         plane_filename = 'openplane/planes/{}.json'.format(self.immatriculation)
 
@@ -122,63 +147,114 @@ class Plane:
                  'W': self.equipements['W'],
                  'X': self.equipements['X'],
                  'Y': self.equipements['Y']
+             },
+             'Centrage': {
+                 'Normale': {
+                    'P1x': self.p1x,
+                    'P1y': self.p1y,
+                    'P2x': self.p2x,
+                    'P2y': self.p2y,
+                    'P3x': self.p3x,
+                    'P3y': self.p3y,
+                    'P4x': self.p4x,
+                    'P4y': self.p4y,
+                    'P5x': self.p5x,
+                    'P5y': self.p5y
+                 },
+                 'Utilitaire': {
+                    'P1x': self.up1x,
+                    'P1y': self.up1y,
+                    'P2x': self.up2x,
+                    'P2y': self.up2y,
+                    'P3x': self.up3x,
+                    'P3y': self.up3y,
+                    'P4x': self.up4x,
+                    'P4y': self.up4y,
+                    'P5x': self.up5x,
+                    'P5y': self.up5y
+                 }
              }}
+
         with open(plane_filename, 'w') as outfile:
             json.dump(plane_values, outfile, indent=4)
 
     def import_plane(self, filepath):
+        values = []
+
         with open(filepath, 'r') as reader:
             datas = json.load(reader)
 
-            self.immatriculation = datas['Immatriculation'].upper()
-            self.plane_type = datas['Type'].upper()
-            self.oaci = datas['OACI'].upper()
-            self.colors = datas['Couleurs']
+            values.append(datas['Immatriculation'].upper())
+            values.append(datas['Type'].upper())
+            values.append(datas['OACI'].upper())
+            values.append(datas['Couleurs'])
 
-            self.s_croisiere = datas['Vitesse']['Croisiere']
-            self.s_montee = datas['Vitesse']['Montee']
-            self.s_descente = datas['Vitesse']['VzMontee']
-            self.v_montee = datas['Vitesse']['Descente']
-            self.v_descente = datas['Vitesse']['VzDescente']
+            values.append(datas['Vitesse']['Croisiere'])
+            values.append(datas['Vitesse']['Montee'])
+            values.append(datas['Vitesse']['VzMontee'])
+            values.append(datas['Vitesse']['Descente'])
+            values.append(datas['Vitesse']['VzDescente'])
 
-            self.vso = datas['Vitesse']['VSO']
-            self.vfe = datas['Vitesse']['VFE']
-            self.vno = datas['Vitesse']['VNO']
-            self.vne = datas['Vitesse']['VNE']
-            self.vx = datas['Vitesse']['Vx']
-            self.vy = datas['Vitesse']['Vy']
+            values.append(datas['Vitesse']['VSO'])
+            values.append(datas['Vitesse']['VFE'])
+            values.append(datas['Vitesse']['VNO'])
+            values.append(datas['Vitesse']['VNE'])
+            values.append(datas['Vitesse']['Vx'])
+            values.append(datas['Vitesse']['Vy'])
 
-            self.carbu_capacity = datas['Carburant']['Capacite']
-            self.carbu_unit = datas['Carburant']['Unite']
-            self.carbu_type = datas['Carburant']['Type']
-            self.carbu_inutilisable = datas['Carburant']['Inutilisable']
-            self.carbu_consom = datas['Carburant']['Consomation']
+            values.append(datas['Carburant']['Capacite'])
+            values.append(datas['Carburant']['Unite'])
+            values.append(datas['Carburant']['Type'])
+            values.append(datas['Carburant']['Inutilisable'])
+            values.append(datas['Carburant']['Consomation'])
 
-            self.piste_dure = datas['Piste']['Dure']
-            self.piste_herbe = datas['Piste']['Herbe']
-            self.piste_unit = datas['Piste']['Unite']
+            values.append(datas['Piste']['Dure'])
+            values.append(datas['Piste']['Herbe'])
+            values.append(datas['Piste']['Unite'])
 
-            self.rdba = datas['RDBA']
-            self.transpondeur = datas['Transpondeur']
-            self.turbulance = datas['Turbulance']
-            self.certification = datas['Certification']
+            values.append(datas['RDBA'])
+            values.append(datas['Transpondeur'])
+            values.append(datas['Turbulance'])
+            values.append(datas['Certification'])
 
-            self.equipements = {
-                "C": datas['Equipement']['C'],
-                "D": datas['Equipement']['D'],
-                "F": datas['Equipement']['F'],
-                "G": datas['Equipement']['G'],
-                "H": datas['Equipement']['H'],
-                "I": datas['Equipement']['I'],
-                "J": datas['Equipement']['J'],
-                "K": datas['Equipement']['K'],
-                "L": datas['Equipement']['L'],
-                "O": datas['Equipement']['O'],
-                "R": datas['Equipement']['R'],
-                "T": datas['Equipement']['T'],
-                "U": datas['Equipement']['U'],
-                "V": datas['Equipement']['V'],
-                "W": datas['Equipement']['W'],
-                "X": datas['Equipement']['X'],
-                "Y": datas['Equipement']['Y']
-            }
+            values.append(datas['Equipement']['C'])
+            values.append(datas['Equipement']['D'])
+            values.append(datas['Equipement']['F'])
+            values.append(datas['Equipement']['G'])
+            values.append(datas['Equipement']['H'])
+            values.append(datas['Equipement']['I'])
+            values.append(datas['Equipement']['J'])
+            values.append(datas['Equipement']['K'])
+            values.append(datas['Equipement']['L'])
+            values.append(datas['Equipement']['O'])
+            values.append(datas['Equipement']['R'])
+            values.append(datas['Equipement']['T'])
+            values.append(datas['Equipement']['U'])
+            values.append(datas['Equipement']['V'])
+            values.append(datas['Equipement']['W'])
+            values.append(datas['Equipement']['X'])
+            values.append(datas['Equipement']['Y'])
+
+            values.append(datas['Centrage']['Normale']['P1x'])
+            values.append(datas['Centrage']['Normale']['P1y'])
+            values.append(datas['Centrage']['Normale']['P2x'])
+            values.append(datas['Centrage']['Normale']['P2y'])
+            values.append(datas['Centrage']['Normale']['P3x'])
+            values.append(datas['Centrage']['Normale']['P3y'])
+            values.append(datas['Centrage']['Normale']['P4x'])
+            values.append(datas['Centrage']['Normale']['P4y'])
+            values.append(datas['Centrage']['Normale']['P5x'])
+            values.append(datas['Centrage']['Normale']['P5y'])
+
+            values.append(datas['Centrage']['Utilitaire']['P1x'])
+            values.append(datas['Centrage']['Utilitaire']['P1y'])
+            values.append(datas['Centrage']['Utilitaire']['P2x'])
+            values.append(datas['Centrage']['Utilitaire']['P2y'])
+            values.append(datas['Centrage']['Utilitaire']['P3x'])
+            values.append(datas['Centrage']['Utilitaire']['P3y'])
+            values.append(datas['Centrage']['Utilitaire']['P4x'])
+            values.append(datas['Centrage']['Utilitaire']['P4y'])
+            values.append(datas['Centrage']['Utilitaire']['P5x'])
+            values.append(datas['Centrage']['Utilitaire']['P5y'])
+
+        self.create_plane(values)
